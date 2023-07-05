84623c1554bfdeb809168a412c24caae89aec073
KromiKF <80040745+KromiKF@users.noreply.github.com>
2023-07-05 11:03:41 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -5,9 +5,9 @@ function move()
 {
     var rand = Math.floor(Math.random()*1280);
     console.log(rand);
-    document.getElementById("CheSchifo").style.marginLeft = rand + "px";
+    document.getElementById("Tasto").style.marginLeft = rand + "px";
     var randy = Math.floor(Math.random()*300);
-    document.getElementById("CheSchifo").style.marginTop = randy + "px";
+    document.getElementById("Tasto").style.marginTop = randy + "px";
 }
 
 function jumpscare()
@@ -15,7 +15,7 @@ function jumpscare()
 
     var audio = new Audio("party2.mp3");
     aleert();
-    document.getElementById("Droga").style.display = "block";
+    document.getElementById("Tasto2").style.display = "block";
     audio.play();
 }
 
